590ceb543d1add9823bc2b028cab8b6a3a12faff
gareth@cerebralfix.com <gareth@cerebralfix.com@f2ea189c-1505-682e-48a4-40892a83d237>
2013-01-26 04:09:37 +0000

    Tidied up files I shouldn't have committed.

---
+++ code/Server/AtriumServer/AtriumServer/User.cpp
@@ -0,0 +1,32 @@
+//
+//  Atrium
+//  Gareth Falkingham
+//
+//  2013(c) - GNU GPL V3
+//
+//  File Name   :   user.cpp
+//  Description :   User
+//  Author      :   Gareth Falkingham
+// 
+
+// Library Includes
+
+// Local Includes
+
+// This Includes
+#include "User.h"
+
+// Static Variables
+
+// Static function prototypes
+
+// Implementation
+
+User::User()
+{
+}
+
+User::~User()
+{
+}
+
---
+++ code/Server/AtriumServer/AtriumServer/User.h
@@ -0,0 +1,51 @@
+//
+//  Atrium
+//  Gareth Falkingham
+//
+//  2013(c) - GNU GPL V3
+//
+//  File Name   :   user.h
+//  Description :   User
+//  Author      :   Gareth Falkingham
+//
+
+#ifndef __ATRIUM_GJ_USER_H__
+#define __ATRIUM_GJ_USER_H__
+
+// Library Includes
+
+// Local Includes
+
+// Types
+
+// Constants
+
+// Prototypes
+
+class User
+{	
+// Member Functions
+public:
+	User();
+	~User();
+
+protected:
+
+private:
+
+// Member Variables
+public:
+
+protected:
+
+private:
+	float m_positionX;
+	float m_positionY;
+
+	int m_userID;
+
+
+
+};
+
+#endif // __ATRIUM_GJ_USER_H__
